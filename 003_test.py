1dfc9c15d7b47fc8ec9f4985540b20b7175e019e
Ke, Liang-Ying <72992250+liangying-Ke@users.noreply.github.com>
2023-07-24 04:32:07 +0800

    Update 003_test.py

--- 003_test.py
+++ 003_test.py
@@ -49,17 +49,14 @@ def test(*kwargs):
     DataLoader = kwargs[-1]
     count = 0
     correct = 0
-    # 設定模型模式
     model.eval()
     start = time.time()
-    # 迭代訓練資料集
     for inputs, targets in tqdm(DataLoader):
         inputs = inputs.to(args.device)
         targets = targets.to(args.device)
         with torch.set_grad_enabled(False):
             _, outputs  = model(inputs)
             outputs = F.sigmoid(outputs)
-            # 取得預測的標籤
             confidence, pred = torch.max(outputs, dim=1)
             for idx, conf in enumerate(confidence):
                 if conf >= 0.5 and pred[idx] == targets[idx]:
@@ -72,20 +69,16 @@ def test(*kwargs):
 def main(args):
     for data_type in args.data_type:
         test_dataset = datasets.ImagesDataset(args=args, data_type=data_type, phase='test')
-        # 取得模型，並且放入cuda
         model = _get_model(args)
         input1 = torch.zeros([1, 3, args.img_size, args.img_size], device=args.device)
         print(summary(model, input1, show_input=False))
-        # 模型可視化
         macs, _ = get_model_complexity_info(model, (3, args.img_size, args.img_size), as_strings=True, print_per_layer_stat=False)
         print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
         print('='*100)
         for metrics in ['F1', 'Acc', 'Loss']:
-            # 取得權重
             path = os.path.join(args.root_model, data_type) if data_type is not None else args.root_model
             weights = torch.load(os.path.join(path, 'switchDBB', f"Backbone_ckpt.best{metrics}.pth.tar"))
             Test_DataLoader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers, pin_memory=True)
-            # 將權重匯入模型
             model.load_state_dict(weights['model_state_dict']) 
             acc, cost_time = test(args, model, Test_DataLoader)
             print(f'data_type: {data_type}, Metrics: {metrics}')
